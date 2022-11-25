bb4f85e6ae5210e08a80d9ba18f349636577ab9e
SharadinduPaul <sharadindu.paul007@gmail.com>
2022-11-25 19:33:10 +0530

    web3 connectivity and minting functions inplemented

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import Web3 from "web3";
 import "./style.css";
+import { ABI, ADDRESS } from "../../data";
 
 import mintPng from "../../assets/mint.png";
 import connectingPng from "../../assets/connecting.png";
@@ -9,27 +11,69 @@ export const Home = () => {
   const [number, setNumber] = useState<number>(0);
   const [connect, setConnect] = useState<boolean>(false);
   const [mint, setMint] = useState<boolean>(false);
+  const [walletAddress, setWalletAddress] = useState<string>("");
 
   useEffect(() => {
     setTimeout(() => {
       if (connect) {
         setConnect(false);
       }
+    }, 3000);
+  }, [connect]);
+  useEffect(() => {
+    setTimeout(() => {
       if (mint) {
         setMint(false);
       }
     }, 3000);
-  }, [connect, mint]);
+  }, [mint]);
 
   const green = "#47ff4a";
+
+  let contract: any;
+  let account: string;
 
   const handleConnectToWallet = async () => {
     if (connect === true) return;
     setConnect(true);
+
+    try {
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+        await window.ethereum.send("eth_requestAccounts");
+        const accounts = await web3.eth.getAccounts();
+        account = accounts[0];
+        console.log(accounts[0]);
+        setWalletAddress(account);
+
+        contract = new web3.eth.Contract(ABI, ADDRESS);
+      }
+    } catch (e) {
+      console.log("error:", e);
+    }
   };
   const handleMint = async () => {
     if (mint === true) return;
     setMint(true);
+
+    if (!walletAddress) {
+      alert("connect to wallet first");
+      return;
+    }
+    if (window.ethereum) {
+      try {
+        const web3 = new Web3(window.ethereum);
+        contract = new web3.eth.Contract(ABI, ADDRESS);
+        const mintRate = Number(await contract.methods.cost().call());
+        const totalAmount = mintRate * number;
+        console.log(walletAddress, number, totalAmount);
+        contract.methods
+          .mint(walletAddress, number)
+          .send({ from: walletAddress, value: String(totalAmount) });
+      } catch (e) {
+        console.log("error", e);
+      }
+    }
   };
 
   return (
@@ -38,7 +82,12 @@ export const Home = () => {
         <h3>
           Connect to your wallet <br /> and start minting NFTs
         </h3>
-        <input type="text" disabled value="Your wallet address" />
+        <input
+          type="text"
+          disabled
+          placeholder="Wallet address"
+          value={walletAddress}
+        />
         <button
           style={{
             backgroundColor: connect ? green : "#c0e0ff",
